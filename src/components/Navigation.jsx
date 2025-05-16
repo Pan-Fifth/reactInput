504919e1cc8ad80ec208f7e4d8e7811ab56825a3
Pan-Fifth <thanakorn.n845@gmail.com>
2025-05-16 12:36:43 +0700

    rename nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,8 +6,8 @@ function Navigation() {
       <Link to ='/' style={{color:"white"}}>Form</Link>
       <Link to ='/cart' style={{color:"white"}}>Cart</Link>
       <Link to ='/search' style={{color:"white"}}>Search</Link>
-      <Link to ='/image' style={{color:"white"}}>Image</Link>
-      <Link to ='/toggle' style={{color:"white"}}>Toggle</Link>
+      <Link to ='/image' style={{color:"white"}}>Image Carousel</Link>
+      <Link to ='/toggle' style={{color:"white"}}>Toggle Theme</Link>
     </nav>
 
   )
